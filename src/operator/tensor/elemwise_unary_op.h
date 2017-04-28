ead8e835451364d1868ac63c9cb22eb4441832bd
reminisce <wujun.nju@gmail.com>
2017-04-28 13:54:51 -0700

    Add cast from dns to csr

--- src/operator/tensor/elemwise_unary_op.h
+++ src/operator/tensor/elemwise_unary_op.h
@@ -167,7 +167,7 @@ struct CastStorageParam : public dmlc::Parameter<CastStorageParam> {
  * by row_idx[cur_row_id] = cur_row_id. Otherwise,
  * row_idx[cur_row_id] = num_rows.
  */
-struct FillRowIdx {
+struct FillRspRowIdx {
   template<typename DType, typename RType>
   MSHADOW_XINLINE static void Map(int i, RType* row_idx, const DType* arr,
                                   const int num_rows, const int num_cols) {
@@ -185,9 +185,9 @@ struct FillRowIdx {
 /*!
  * \brief
  * Given a DNS storage type tensor, create a RSP type sparse tensor
- * from it. This would deep-copy non-zero rows of the dense vector to
- * the data blob of the RSP tensor and allocate memory for storing the
- * non-zero row indices in the RSP tensor.
+ * from it. This would allocate memory for storing the row idx and
+ * non-zero rows for the rsp and deep-copy non-zero rows of the
+ * dns to the rsp data blob.
  * TODO(junwu): The argument type for the dense ndarray is TBlob instead
  * of NDArray since it's convenient to call this function from any
  * operator's Forward/Backward functions where dev_id is unknown
@@ -209,7 +209,7 @@ void CastStorageDnsRspImpl(const OpContext& ctx, const TBlob& dns, NDArray* rsp)
       const index_t num_rows = dns.shape_[0];
       const index_t num_cols = dns.shape_[1];
       // Fill input_data.shape_[0] into row_idx array
-      mxnet_op::Kernel<FillRowIdx, xpu>::Launch(s, num_rows, row_idx, dns.dptr<DType>(),
+      mxnet_op::Kernel<FillRspRowIdx, xpu>::Launch(s, num_rows, row_idx, dns.dptr<DType>(),
           num_rows, num_cols);
 
       // single thread scanning row_idx array to find out number of non-zero rows
@@ -236,39 +236,129 @@ void CastStorageDnsRspImpl(const OpContext& ctx, const TBlob& dns, NDArray* rsp)
   });
 }
 
-template<typename xpu>
-void CastStorageComputeRspDns(const nnvm::NodeAttrs& attrs,
-                 const OpContext& ctx,
-                 const std::vector<NDArray>& inputs,
-                 const std::vector<OpReqType>& req,
-                 const std::vector<NDArray>& outputs) {
-  CHECK_EQ(inputs.size(), 1U);
-  CHECK_EQ(outputs.size(), 1U);
-  if (inputs[0].storage_type() == kDefaultStorage
-      && outputs[0].storage_type() == kRowSparseStorage) {
-    NDArray tmp = outputs[0];  // get rid of the const qualifer
-    CastStorageDnsRspImpl<xpu>(ctx, inputs[0].data(), &tmp);
+/*!
+ * \brief This is the kernel for initializing the indptr in a csr tensor.
+ */
+struct FillCsrIndPtr {
+  /*!
+   * \brief
+   * \param i the i-th row of the dns tensor
+   * \param indptr indptr of the csr tensor
+   * \param dns the dns tensor
+   * \param num_rows
+   * \param num_cols
+   */
+  template<typename DType, typename IType>
+  MSHADOW_XINLINE static void Map(int i, IType* indptr, const DType* dns,
+                                  const int num_rows, const int num_cols) {
+    indptr[i+1] = 0;
+    const int offset = i * num_cols;
+    for (int j = 0; j < num_cols; ++j) {
+      if (dns[offset+j] != 0) {
+        ++indptr[i+1];
+      }
+    }
   }
-  // TODO(junwu): move the following code to a impl function
+};
+
+/*!
+ * \brief This is the kernel for initializing the col_idx and value array
+ * of the csr tensor
+ */
+struct FillCsrColIdxAndVals {
+  /*!
+   * \brief
+   * \param i the i-th row of the dns tensor
+   * \param val value array of the csr
+   * \param col_idx column idx array of the csr
+   * \param indptr indptr array of the csr
+   * \param dns the dns tensor
+   * \param num_rows number of rows of the dns
+   * \param num_cols number of columns of the dns
+   */
+  template<typename DType, typename IType, typename CType>
+  MSHADOW_XINLINE static void Map(int i, DType* val, CType* col_idx,
+                                  const IType* indptr, const DType* dns,
+                                  const int num_rows, const int num_cols) {
+    const int offset = i * num_cols;
+    int k = indptr[i];
+    for (int j = 0; j < num_cols; ++j) {
+      if (dns[offset+j] != 0) {
+        val[k] = dns[offset+j];
+        col_idx[k] = j;
+        ++k;
+      }
+    }
+  }
+};
+
+/*!
+ * \brief
+ * Given a DNS storage type tensor, create a CSR type sparse tensor from it.
+ * This would allocate memory for storing the indptr, values, and column idx
+ * of the csr and copy the non-zero values to the value array in the csr.
+ * TODO(junwu): The argument type for the dense ndarray is TBlob instead
+ * of NDArray since it's convenient to call this function from any
+ * operator's Forward/Backward functions where dev_id is unknown
+ * but required to wrap a TBlob object as an NDArray. See the use case
+ * in DotForwardCsrDnsRsp in matrix_op-inl.h.
+ * Will revisit this interface in the future.
+ */
+template<typename xpu>
+void CastStorageDnsCsrImpl(const OpContext& ctx, const TBlob& dns, NDArray* csr) {
+  CHECK(csr != nullptr);
+  CHECK_EQ(csr->storage_type(), kCSRStorage);
+  CHECK_EQ(dns.shape_.ndim(), 2);
+  CHECK_EQ(dns.shape_, csr->shape());
+
+  mshadow::Stream<xpu> *s = ctx.get_stream<xpu>();
+  MSHADOW_TYPE_SWITCH(dns.type_flag_, DType, {  // data type
+    NDARRAY_IDX_TYPE_SWITCH(csr->aux_type(csr::kIndPtr), IType, {  // indptr type
+      NDARRAY_IDX_TYPE_SWITCH(csr->aux_type(csr::kIdx), CType, {  // col idx type
+        const index_t num_rows = dns.shape_[0];
+        const index_t num_cols = dns.shape_[1];
+        csr->CheckAndAllocAuxData(csr::kIndPtr, TShape({num_rows+1}));
+        IType* indptr = csr->aux_data(csr::kIndPtr).dptr<IType>();
+        DType* dns_data = dns.dptr<DType>();
+        mxnet_op::Kernel<FillCsrIndPtr, xpu>::Launch(s, num_rows, indptr,
+            dns_data, num_rows, num_cols);
+        // single thread to accumulate indptr
+        // indptr[num_rows] indicates the number of non-zero elements
+        indptr[0] = 0;
+        for (index_t i = 0; i < num_rows; ++i) {
+          indptr[i+1] += indptr[i];
+        }
+        // allocate column idx array and value array
+        csr->CheckAndAllocAuxData(csr::kIdx, TShape({static_cast<index_t>(indptr[num_rows])}));
+        csr->CheckAndAllocData(TShape({static_cast<index_t>(indptr[num_rows])}));
+        // fill col_idx and value arrays of the csr
+        mxnet_op::Kernel<FillCsrColIdxAndVals, xpu>::Launch(s, num_rows,
+            csr->data().dptr<DType>(), csr->aux_data(csr::kIdx).dptr<CType>(),
+            indptr, dns_data, num_rows, num_cols);
+      });
+    });
+  });
+}
+
+/*!
+ * \brief This function assumes that the meomry for dns has been allocated already
+ * since the shape is known at binding stage.
+ */
+template<typename xpu>
+void CastStorageRspDnsImpl(const OpContext& ctx, const NDArray& rsp, TBlob* dns) {
   using namespace mshadow;
   using namespace mshadow::expr;
   Stream<xpu> *s = ctx.get_stream<xpu>();
-  auto out = outputs[0];
-  auto in = inputs[0];
-  auto stype = in.storage_type();
-  CHECK_EQ(stype, kRowSparseStorage);
-  CHECK_EQ(out.storage_type(), kDefaultStorage);
-  MSHADOW_TYPE_SWITCH(in.dtype(), DType, {
-    MSHADOW_TYPE_SWITCH(in.aux_type(rowsparse::kIdx), IType, {
-      // Fill in zeros. SLOW
-      out.data().FlatTo1D<xpu, DType>(s) = 0;
+  CHECK_EQ(rsp.storage_type(), kRowSparseStorage);
+  MSHADOW_TYPE_SWITCH(dns->type_flag_, DType, {
+    MSHADOW_TYPE_SWITCH(rsp.aux_type(rowsparse::kIdx), IType, {
       // data() is not empty
-      if (in.storage_shape().ndim() != 0) {
+      if (rsp.storage_shape().ndim() != 0) {
         // Copy over
-        auto in_data = in.data().FlatTo2D<xpu, DType>(s);
-        auto out_data = out.data().FlatTo2D<xpu, DType>(s);
-        auto num_rows = in.aux_shape(rowsparse::kIdx)[0];
-        auto in_idx = in.aux_data(rowsparse::kIdx).FlatTo1D<xpu, IType>(s);
+        auto in_data = rsp.data().FlatTo2D<xpu, DType>(s);
+        auto out_data = dns->FlatTo2D<xpu, DType>(s);
+        auto num_rows = rsp.aux_shape(rowsparse::kIdx)[0];
+        auto in_idx = rsp.aux_data(rowsparse::kIdx).FlatTo1D<xpu, IType>(s);
         for (size_t i = 0; i < num_rows; i += 1) {
           mshadow::Copy(out_data[in_idx[i]], in_data[i], s);
         }
@@ -279,19 +369,26 @@ void CastStorageComputeRspDns(const nnvm::NodeAttrs& attrs,
 
 template<typename xpu>
 void CastStorageComputeEx(const nnvm::NodeAttrs& attrs,
-                 const OpContext& ctx,
-                 const std::vector<NDArray>& inputs,
-                 const std::vector<OpReqType>& req,
-                 const std::vector<NDArray>& outputs) {
+                          const OpContext& ctx,
+                          const std::vector<NDArray>& inputs,
+                          const std::vector<OpReqType>& req,
+                          const std::vector<NDArray>& outputs) {
   using namespace mshadow;
   using namespace mshadow::expr;
   Stream<xpu> *s = ctx.get_stream<xpu>();
   CHECK_EQ(inputs.size(), 1);
   CHECK_EQ(outputs.size(), 1);
-  auto stype = inputs[0].storage_type();
-  auto out_stype = outputs[0].storage_type();
-  if (stype == kRowSparseStorage && out_stype == kDefaultStorage) {
-    CastStorageComputeRspDns<xpu>(attrs, ctx, inputs, req, outputs);
+  const auto src_stype = inputs[0].storage_type();
+  const auto dst_stype = outputs[0].storage_type();
+  if (src_stype == kRowSparseStorage && dst_stype == kDefaultStorage) {
+    TBlob ret = outputs[0].data();
+    CastStorageRspDnsImpl<xpu>(ctx, inputs[0], &ret);
+  } else if (src_stype == kDefaultStorage && dst_stype == kRowSparseStorage) {
+    NDArray ret = outputs[0];  // get rid of the const qualifer
+    CastStorageDnsRspImpl<xpu>(ctx, inputs[0].data(), &ret);
+  } else if (src_stype == kDefaultStorage && dst_stype == kCSRStorage) {
+    NDArray ret = outputs[0];  // get rid of the const qualifer
+    CastStorageDnsCsrImpl<xpu>(ctx, inputs[0].data(), &ret);
   } else {
     LOG(FATAL) << "Not implemented";
   }
