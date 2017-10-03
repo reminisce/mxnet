248704ad8dc3ca0604b72978698ca34c6f4290ee
reminisce <wujun.nju@gmail.com>
2017-10-03 11:14:21 -0700

    Add layout to benchmark_convolution

--- benchmark/python/quantization/benchmark_op.py
+++ benchmark/python/quantization/benchmark_op.py
@@ -2,12 +2,12 @@ import mxnet as mx
 from mxnet.test_utils import check_speed
 
 
-def benchmark_convolution(data_shape, kernel, num_filter, pad, stride, no_bias=True, repeats=20):
+def benchmark_convolution(data_shape, kernel, num_filter, pad, stride, no_bias=True, layout='NCHW', repeats=20):
     ctx_gpu = mx.gpu(0)
     data = mx.sym.Variable(name="data", shape=data_shape)
     # conv cudnn
     conv_cudnn = mx.sym.Convolution(data=data, kernel=kernel, num_filter=num_filter, pad=pad, stride=stride,
-                                    no_bias=no_bias, cudnn_off=False, name="conv_cudnn")
+                                    no_bias=no_bias, layout=layout, cudnn_off=False, name="conv_cudnn")
     arg_shapes, _, _ = conv_cudnn.infer_shape(data=data_shape)
     input_data = mx.nd.random.normal(0, 0.2, shape=data_shape, ctx=ctx_gpu)
     conv_weight_name = conv_cudnn.list_arguments()[1]
@@ -25,7 +25,7 @@ def benchmark_convolution(data_shape, kernel, num_filter, pad, stride, no_bias=T
     quantized_conv2d = mx.sym.quantized_conv2d(data=qdata, weight=weight, min_data=min_data, max_data=max_data,
                                                min_weight=min_weight, max_weight=max_weight,
                                                kernel=kernel, num_filter=num_filter, pad=pad, stride=stride,
-                                               no_bias=no_bias, cudnn_off=False, name='quantized_conv2d')
+                                               no_bias=no_bias, layout=layout, cudnn_off=False, name='quantized_conv2d')
     qargs = {qdata.name: mx.quantization.quantize(input_data)[0],
              min_data.name: mx.quantization.quantize(input_data)[1],
              max_data.name: mx.quantization.quantize(input_data)[2],
@@ -36,8 +36,8 @@ def benchmark_convolution(data_shape, kernel, num_filter, pad, stride, no_bias=T
                              grad_req='null', typ='forward') * 1000
 
     print('==================================================================================================')
-    print('data=%s, kernel=%s, num_filter=%s, pad=%s, stride=%s, no_bias=%s, repeats=%s'
-          % (data_shape, kernel, num_filter, pad, stride, no_bias, repeats))
+    print('data=%s, kernel=%s, num_filter=%s, pad=%s, stride=%s, no_bias=%s, layout=%s, repeats=%s'
+          % (data_shape, kernel, num_filter, pad, stride, no_bias, layout, repeats))
     print('%s , ctx=%s, time=%.2f ms' % (conv_cudnn.name+'-FP32', ctx_gpu, conv_cudnn_time))
     print('%s, ctx=%s, time=%.2f ms' % (quantized_conv2d.name, ctx_gpu, qconv_time))
     print('quantization speedup:               %.1fX' % (conv_cudnn_time / qconv_time))
@@ -47,14 +47,31 @@ def benchmark_convolution(data_shape, kernel, num_filter, pad, stride, no_bias=T
 if __name__ == '__main__':
     for batch_size in [32, 64, 128]:
         benchmark_convolution(data_shape=(batch_size, 64, 122, 122), kernel=(7, 7), num_filter=64,
-                              pad=(1, 1), stride=(1, 1), repeats=20)
+                              pad=(1, 1), stride=(1, 1), layout='NCHW', repeats=20)
+        benchmark_convolution(data_shape=(batch_size, 122, 122, 64), kernel=(7, 7), num_filter=64,
+                              pad=(1, 1), stride=(1, 1), layout='NHWC', repeats=20)
+
         benchmark_convolution(data_shape=(batch_size, 64, 56, 56), kernel=(3, 3), num_filter=64,
-                              pad=(3, 3), stride=(2, 2), repeats=20)
+                              pad=(3, 3), stride=(2, 2), layout='NCHW', repeats=20)
+        benchmark_convolution(data_shape=(batch_size, 56, 56, 64), kernel=(3, 3), num_filter=64,
+                              pad=(3, 3), stride=(2, 2), layout='NHWC', repeats=20)
+
         benchmark_convolution(data_shape=(batch_size, 256, 56, 56), kernel=(1, 1), num_filter=256,
-                              pad=(0, 0), stride=(1, 1), repeats=20)
+                              pad=(0, 0), stride=(1, 1), layout='NCHW', repeats=20)
+        benchmark_convolution(data_shape=(batch_size, 56, 56, 256), kernel=(1, 1), num_filter=256,
+                              pad=(0, 0), stride=(1, 1), layout='NHWC', repeats=20)
+
         benchmark_convolution(data_shape=(batch_size, 128, 28, 28), kernel=(3, 3), num_filter=128,
-                              pad=(1, 1), stride=(1, 1), repeats=20)
+                              pad=(1, 1), stride=(1, 1), layout='NCHW', repeats=20)
+        benchmark_convolution(data_shape=(batch_size, 28, 28, 128), kernel=(3, 3), num_filter=128,
+                              pad=(1, 1), stride=(1, 1), layout='NHWC', repeats=20)
+
         benchmark_convolution(data_shape=(batch_size, 256, 14, 14), kernel=(3, 3), num_filter=256,
-                              pad=(1, 1), stride=(1, 1), repeats=20)
+                              pad=(1, 1), stride=(1, 1), layout='NCHW', repeats=20)
+        benchmark_convolution(data_shape=(batch_size, 14, 14, 256), kernel=(3, 3), num_filter=256,
+                              pad=(1, 1), stride=(1, 1), layout='NHWC', repeats=20)
+
         benchmark_convolution(data_shape=(batch_size, 2048, 7, 7), kernel=(1, 1), num_filter=2048,
-                              pad=(0, 0), stride=(1, 1), repeats=20)
+                              pad=(0, 0), stride=(1, 1), layout='NCHW', repeats=20)
+        benchmark_convolution(data_shape=(batch_size, 7, 7, 2048), kernel=(1, 1), num_filter=2048,
+                              pad=(0, 0), stride=(1, 1), layout='NHWC', repeats=20)
