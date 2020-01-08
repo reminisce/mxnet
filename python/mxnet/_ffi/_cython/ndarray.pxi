ccac185de6483a4731acbb7ab275474b63ae20ac
Haozheng Fan <fanhaozh@amazon.com>
2020-01-08 22:15:57 +0800

    Implement zeros

--- python/mxnet/_ffi/_cython/ndarray.pxi
+++ python/mxnet/_ffi/_cython/ndarray.pxi
@@ -1,5 +1,6 @@
 import ctypes
 from ...cython.ndarray import _ndarray_cls, _np_ndarray_cls
+from ...numpy import ndarray
 
 # cdef NewArray(NDArrayHandle handle, int stype=-1, int is_np_array=0):
 #     """Create a new array given handle"""
@@ -9,4 +10,6 @@ from ...cython.ndarray import _ndarray_cls, _np_ndarray_cls
 
 cdef c_make_array(void* handle):
     create_array_fn = _np_ndarray_cls
-    return create_array_fn(ctypes.cast(<unsigned long long>handle, ctypes.c_void_p))
+    print(create_array_fn)
+    # return return ndarray(handle=None if value[0].v_handle == 0 else ctypes.cast(value[0].v_handle, NDArrayHandle))
+    return ndarray(handle=ctypes.cast(<unsigned long long>handle, ctypes.c_void_p))
--- src/c_api/c_api_adt.cc
+++ src/c_api/c_api_adt.cc
@@ -3,7 +3,12 @@
 #include <mxnet/runtime/packed_func.h>
 #include <mxnet/c_api_adt.h>
 #include <mxnet/api_registry.h>
+#include <mxnet/base.h>
+#include <nnvm/c_api.h>
 #include <iostream>
+
+#include "../operator/tensor/init_op.h"
+#include "../imperative/imperative_utils.h"
 
 void MXTestADT(size_t ptr) {
   using namespace mxnet::runtime;
@@ -36,7 +41,37 @@ namespace mxnet {
 
 MXNET_REGISTER_API("np.zeros1")
 .set_body([](runtime::MXNetArgs args, runtime::MXNetRetValue* ret) {
+  const nnvm::Op* op = Op::Get("_npi_zeros");
+  const runtime::ObjectRef ref = args[0].operator runtime::ObjectRef();
+  const runtime::ADTObj* obj = ref.as<runtime::ADTObj>();
+  std::cout << "size = " << obj->size << std::endl;
+  for (uint32_t i = 0; i < obj->size; ++i) {
+    int64_t value = obj->operator[](i).as<::mxnet::runtime::IntegerObj>()->value;
+    std::cout << value << " ";
+  }
+  std::cout << std::endl;
+  mxnet::op::InitOpParam param;
+  param.shape = TShape(obj->size, 0);
+  for (uint32_t i = 0; i < obj->size; ++i) {
+    int64_t value = obj->operator[](i).as<::mxnet::runtime::IntegerObj>()->value;
+    param.shape[i] = value;
+  }
+  param.dtype = 0;
+  param.ctx = "cpu";
+  nnvm::NodeAttrs attrs;
+  attrs.parsed = std::move(param);
+  attrs.op = op;
+
+  int num_inputs = 0;
+  int infered_num_outputs;
+  int num_visible_outputs;
+  mxnet::imperative::SetNumOutputs(op, attrs, num_inputs, &infered_num_outputs, &num_visible_outputs);
+
+  std::vector<mxnet::NDArray*> ndoutputs(1, nullptr), ndinputs;
+  ndoutputs[0] = reinterpret_cast<mxnet::NDArray*>(new mxnet::NDArray());
+  auto state = mxnet::Imperative::Get()->Invoke(Context::CPU(), attrs, ndinputs, ndoutputs);
   
+  *ret = reinterpret_cast<mxnet::NDArray*>(ndoutputs[0]);
 });
 
 }  // namespace mxnet
